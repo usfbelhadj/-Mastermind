f73dacdac1f46b8381f158a18188fba8db68667b
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-18 11:55:58 +0100

    incorporate random in main

---
+++ choise.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+"""
+import random
+
+
+def randomize():
+    """ """
+    objective = []
+    for i in range(4):
+        objective.append(random.randint(1, 6))
+    return (objective)
+
+
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-#!/home/achref/Desktop/Holberton/Mastermind/Mastermind/bin/python3
+#!/usr/bin/python3
 """
 """
+from choise import randomize
+
+
 class Mastermind():
-    objective = []
+    objective = randomize()
     playerinput = []
     result = []
     size = 4
     def __init__(self):
         pass
     def Player_Input(self,playerinput, size):
-        for(i in range [0 , size]):
+        for i in range (0 , size):
             playerinput.append(int(input("Put your num => {}".format(i))))
 
+if __name__ == "__main__":
+   print(Mastermind.objective)
