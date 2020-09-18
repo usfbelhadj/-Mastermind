1ce42d553b8ffdb2035c8106123ea39192228011
achref <achrefbs>
2020-09-18 11:49:21 +0100

    playerinput

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ class Mastermind():
     def __init__(self):
         pass
     def Player_Input(self,playerinput, size):
-        for(i in range [0 , size]):
-            playerinput.append(int(input("Put your num => {}".format(i))))
+        for i in range (0, size):
+           Mastermind.playerinput.append(int(input("Put your num ".format(i))))
 
+if __name__ == "__main__":
+    p1 = Mastermind()
+    p1.Player_Input(Mastermind.playerinput,Mastermind.size)
+    print(Mastermind.playerinput)
