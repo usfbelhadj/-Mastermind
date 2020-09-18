e9b7a70ca78bd812d9a6ff0f69e772bfe629538c
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-18 14:05:31 +0100

    fill the comments

--- mastermind.py
+++ mastermind.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 """
+The base class for mastermind(a game logic)
 """
 from choise import randomize
+
+
 class Mastermind():
+    """
+    The main class
+    """
     objective = [1 , 2 , 3 , 4]
     playerinput = []
     result = []
     size = 4
     def __init__(self):
+        """
+        Inisiliase the values
+        """
         self.objective = [1 , 2 , 3 , 4]
         self.playerinput = []
         self.result = []
         self.size = 4
     def Player_Input(self):
+        """
+        Take the input of the player
+        """
         for i in range (1, self.size+1):
             put = (int(input("Put your num [{}] ".format(i))))
             while(len(str(put)) > 1):
@@ -21,11 +33,19 @@ class Mastermind():
             self.playerinput.append(put)
            
     def print_list(self):
+        """
+        print the player input
+        print the counter result
+        print the result
+        """
         print(self.playerinput)
         print(self.objective)
         print(self.result)
 
     def compare(self):
+        """
+        Function to compare betwen to lists
+        """
         self.result = []
         comparelist = self.objective.copy()
         for i in range(len(self.playerinput)):
@@ -38,4 +58,3 @@ class Mastermind():
             else:
                 self.result.append(0)
         self.playerinput = []
-        print(self.result)
--- singleplayer.py
+++ singleplayer.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
+"""
+The single player virsion of the game
+""""
 import random
 from mastermind import Mastermind
 
 class SinglePlayer(Mastermind):
+    """
+    Full the list objictive with random numbers
+    """
     def __init__(self):
+        """
+        inisialise objictive
+        """
         self.objective = SinglePlayer.randomize()
     def randomize():
+        """
+        full the objictive with random numbers
+        """
         objective = []
         for i in range(4):
             objective.append(random.randint(1, 6))
         return (objective)
-
-
-
 
 if __name__ == "__main__":
     i = 0
