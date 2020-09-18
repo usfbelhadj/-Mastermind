08b46b9160e397ce42133b44702d43572f5d7c32
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-18 19:14:56 +0100

    menu page

--- menu.py
+++ menu.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """
 """
+import pygame
+import pygame_menu
 
-class Game():
-    """ """
-    def __init__(self):
-        
+
+pygame.init()
+surface = pygame.display.set_mode((600, 800))
+
+def set_difficulty(value, difficulty):
+    # Do the job here !
+    pass
+
+def start_the_game():
+    # Do the job here !
+    pass
+
+def Play_Mode(mode, value):
+    pass
+
+menu = pygame_menu.Menu(500, 500, 'Master Mind Game',
+                       theme=pygame_menu.themes.THEME_SOLARIZED)
+
+menu.add_text_input('Enter Your Name :', default='Master')
+menu.add_selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add_selector('Play Mode :', [('Single Player', 1), ('Two Players', 2)], onchange=Play_Mode)
+menu.add_button('Play', start_the_game)
+menu.add_button('Quit', pygame_menu.events.EXIT)
+
+menu.mainloop(surface)
