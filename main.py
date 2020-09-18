7069406ff4d9ff3f7310b5cbf9f519d0805bffb6
achref <achrefbs>
2020-09-18 12:11:46 +0100

    add compare

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from choise import randomize
 
 
 class Mastermind():
-    objective = randomize()
+    objective = [1 , 2 , 3 , 4]
     playerinput = []
     result = []
     size = 4
@@ -18,9 +18,21 @@ class Mastermind():
     def print_list(self):
         print(Mastermind.playerinput)
         print(Mastermind.objective)
+        print(Mastermind.result)
+
+    def compare(self, playerinput, objective, result):
+        for i in range(len(Mastermind.playerinput)):
+            if Mastermind.playerinput[i] == Mastermind.objective[i]:
+                Mastermind.result.append(2)
+            elif Mastermind.playerinput[i] in Mastermind.objective:
+                Mastermind.result.append(1)
+            else:
+                Mastermind.result.append(0)
 
 
 if __name__ == "__main__":
     p1 = Mastermind()
     p1.Player_Input(Mastermind.playerinput,Mastermind.size)
+    p1.print_list()
+    p1.compare(Mastermind.playerinput, Mastermind.objective, Mastermind.result)
     p1.print_list()
